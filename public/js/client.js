8560240a5e2cf9c310a2fbe2f54360e2ebaaffff
alberto__segura <alberto.segura.delgado@gmail.com>
2015-03-22 14:32:46 +0100

    Multiple calls. Only receive calls if user status is online. Other bugs fixes

--- public/js/client.js
+++ public/js/client.js
@@ -6,39 +6,33 @@ $('document').ready(function () {
 			//require(['Peer'], function (Peer) {
 				// Listeners for calls
 				socket.on("plugins.incomingCall."+app.user.uid, function(data){
-					console.log(data);
-					app.alert({
-						type: 'success',
-						timeout: 0,
-						title: 'Llamada Entrante',
-						message: ""+data.username+" te esta llamando. <br> <a id='answerCall'>Responder</a> | <a id='closeCall'>Rechazar</a> <video style='display:none;' autoplay id='callStream'></video>",
-						alert_id: 'actualCall'
-					});
-					setTimeout(function(){
-						$("#alert_button_actualCall").find(".close").hide();
-						$("#answerCall").on("click", function(){
-							app.user.peer.actualCallUsername = data.username;
-						    socket.emit("plugins.acceptedIncomingCall", {peerid:app.user.peer.id, youruid:data.uid});
-						    app.alert({
-								type: 'success',
-								timeout: 0,
-								title: 'Llamada en curso',
-								message: "Hablando con "+app.user.peer.actualCallUsername+". <br> <a id='closeCall'>Colgar</a> <video style='display:none;' autoplay id='callStream'></video>",
-								alert_id: 'actualCall'
-							});
-							setTimeout(function(){
-								$("#alert_button_actualCall").find(".close").hide();
-								$("#closeCall").on("click", function(){
-								    app.user.call.close();
-								    $("#alert_button_actualCall").remove();
+					if(app.user.status && app.user.status == "online")
+					{	// Only show incoming calls if im online
+						app.alert({
+							type: 'success',
+							timeout: 0,
+							title: 'Llamada Entrante',
+							message: ""+data.username+" te esta llamando. <br> <a id='answerCall'>Responder</a> | <a id='closeCall'>Rechazar</a> <video style='display:none;' autoplay id='callStream'></video>",
+							alert_id: 'actualCall_'+data.username
+						});
+						setTimeout(function(){
+							$("#alert_button_actualCall").find(".close").hide();
+							$("#answerCall").on("click", function(){
+								app.user.peer.actualCallUsername = data.username;
+							    socket.emit("plugins.acceptedIncomingCall", {peerid:app.user.peer.id, youruid:data.uid});
+							    app.alert({
+									type: 'success',
+									timeout: 7000,
+									title: 'Conectando..',
+									message: "Preparando llamada con "+data.username+" ..",
+									alert_id: 'actualCall_'+data.username
 								});
-							}, 200);
-						});
-						$("#closeCall").on("click", function(){
-							app.user.call.close();
-						    $("#alert_button_actualCall").remove();
-						});
-					}, 200);
+							});
+							$("#closeCall").on("click", function(){
+							    $("#alert_button_actualCall_"+data.username).remove();
+							});
+						}, 200);
+					}
 				});
 
 				socket.on("plugins.acceptedIncomingCall."+app.user.uid, function(data){
@@ -46,28 +40,32 @@ $('document').ready(function () {
 					app.user.peer.actualCallUsername = data.username;
 					navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 					navigator.getUserMedia({video: false, audio: true}, function(stream) {
-					  app.user.call = app.user.peer.call(data.peerid, stream);
-					  app.user.call.answer(stream); // Answer the call with an A/V stream.
-						app.user.call.on('stream', function(remoteStream) {
+					  var call = app.user.peer.call(data.peerid, stream);
+					  app.user.call[call.id] = call;
+					  app.user.call[call.id].answer(stream); // Answer the call with an A/V stream.
+						app.user.call[call.id].on('stream', function(remoteStream) {
 						  // Show stream in some video/canvas element.
-						  $('#callStream').prop('src', URL.createObjectURL(remoteStream));
+						  console.log($('#callStream'));
+						  $("#alert_button_actualCall_"+this.id).find('#callStream').prop('src', URL.createObjectURL(remoteStream));
 						  //console.log("Received stream..");
 						});
-						app.user.call.on("close", function(){
-							$("#alert_button_actualCall").remove();
+						app.user.call[call.id].on("close", function(){
+							var cid = call.id;
+							$("#alert_button_actualCall_"+cid).remove();
 						});
-					  app.alert({
+					  	app.alert({
 							type: 'success',
 							timeout: 0,
 							title: 'Llamada en curso',
-							message: "Hablando con "+app.user.peer.actualCallUsername+". <br> <a id='closeCall'>Colgar</a> <video style='display:none;' autoplay id='callStream'></video>",
-							alert_id: 'actualCall'
+							message: "Hablando con "+data.username+". <br> <a id='closeCall'>Colgar</a> <video style='display:none;' autoplay id='callStream'></video>",
+							alert_id: 'actualCall_'+call.id
 						});
 					  	setTimeout(function(){
 					  		$("#alert_button_actualCall").find(".close").hide();
 							$("#closeCall").on("click", function(){
-							    app.user.call.close();
-							    $("#alert_button_actualCall").remove();
+								var cid = call.id;
+							    app.user.call[cid].close();
+							    $("#alert_button_actualCall_"+cid).remove();
 							});
 						}, 200);
 					}, function(err) {
@@ -88,24 +86,43 @@ $('document').ready(function () {
 
 				// Change it with your api key!!
 				app.user.peer = new Peer({ key: 'lwjd5qra8257b9', debug: 3});
+				app.user.call = {};
 
 				// Prepare for call answer
 				navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 				app.user.peer.on('call', function(call) {
-					app.user.call = call;
-				  navigator.getUserMedia({video: false, audio: true}, function(stream) {
-				  	call.answer(stream); // Answer the call with an A/V stream.
-				    call.on('stream', function(remoteStream) {
-				      // Show stream in some video/canvas element.
-				      $('#callStream').prop('src', URL.createObjectURL(remoteStream));
-				      //console.log("Received stream..");
-				    });
-				    call.on("close", function(){
-						$("#alert_button_actualCall").remove();
+					app.user.call[call.id] = call;
+				  	navigator.getUserMedia({video: false, audio: true}, function(stream) {
+					  	call.answer(stream); // Answer the call with an A/V stream.
+					  	app.user.call[call.id] = call;
+						app.user.call[call.id].answer(stream); // Answer the call with an A/V stream.
+						app.user.call[call.id].on('stream', function(remoteStream) {
+						  // Show stream in some video/canvas element.
+						  $("#alert_button_actualCall_"+this.id).find('#callStream').prop('src', URL.createObjectURL(remoteStream));
+						  //console.log("Received stream..");
+						});
+						app.user.call[call.id].on("close", function(){
+							var cid = call.id;
+							$("#alert_button_actualCall_"+cid).remove();
+						});
+					  	app.alert({
+							type: 'success',
+							timeout: 0,
+							title: 'Llamada en curso',
+							message: "Hablando con "+app.user.peer.actualCallUsername+". <br> <a id='closeCall'>Colgar</a> <video style='display:none;' autoplay id='callStream'></video>",
+							alert_id: 'actualCall_'+call.id
+						});
+					  	setTimeout(function(){
+					  		$("#alert_button_actualCall").find(".close").hide();
+							$("#closeCall").on("click", function(){
+								var cid = call.id;
+							    app.user.call[cid].close();
+							    $("#alert_button_actualCall_"+cid).remove();
+							});
+						}, 200);
+					}, function(err) {
+					  console.log('Failed to get local stream' ,err);
 					});
-				  }, function(err) {
-				    console.log('Failed to get local stream' ,err);
-				  });
 				});
 			//});
 		}
@@ -116,7 +133,7 @@ $('document').ready(function () {
 			socket.emit("plugins.callUser", {uid:uid}, function(err,data){});
 			app.alert({
 				type: 'success',
-				timeout: 0,
+				timeout: 3000,
 				title: 'Llamando..',
 				message: "Si responde a la llamada deberás permitir el acceso al micro y empezar a hablar con el/ella.",
 				alert_id: 'actualCall'
